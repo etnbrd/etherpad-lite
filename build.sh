c7351d0ad8383ba99dabe0194b98dc7d80f6d19b
etnbrd <etn@etnbrd.com>
2016-02-03 00:17:08 +0100

    fixed typo

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p node_modules
-[ -e node_modules/ep_etherpad-lite ] || ln -s src node_modulesep_etherpad-lite
+[ -e node_modules/ep_etherpad-lite ] || ln -s src node_modules/ep_etherpad-lite
 
 sed -i s/ADMIN_PASSWD/$ADMIN_PASSWD/g settings.json
 sed -i s/USER_PASSWD/$USER_PASSWD/g   settings.json
