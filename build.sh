22da5ad70a4c4a52998a1372329b0e1d4f804221
etnbrd <etn@etnbrd.com>
2016-02-03 00:30:10 +0100

    fix problem with symlink creation

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 mkdir -p node_modules
-[ -e node_modules/ep_etherpad-lite ] || ln -s src node_modules/ep_etherpad-lite
+cd node_modules
+[ -eep_etherpad-lite ] || ln -s ../src ep_etherpad-lite
+cd ..
 
 sed -i s/ADMIN_PASSWD/$ADMIN_PASSWD/g settings.json
 sed -i s/USER_PASSWD/$USER_PASSWD/g   settings.json
