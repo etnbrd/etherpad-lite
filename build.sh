e69b4a5990f21a8173f5ad6cac93c2f747e8911f
etnbrd <etn@etnbrd.com>
2016-02-03 00:57:51 +0100

    fix build again

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 mkdir -p node_modules
 cd node_modules
-[ -eep_etherpad-lite ] || ln -s ../src ep_etherpad-lite
+[ -e ep_etherpad-lite ] || ln -s ../src ep_etherpad-lite
+cd ../src
+[ -e node_modules ] || ln -s ../node_modules node_modules
 cd ..
 
 sed -i s/ADMIN_PASSWD/$ADMIN_PASSWD/g settings.json
